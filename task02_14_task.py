7f7be6739853320b9b45e9b09921550787d4ddfe
Pylyp Dukhliy <pylyp@pylyp.com.ua>
2019-04-15 22:57:19 +0300

    Add files via upload

---
+++ task02_14_task.py
@@ -0,0 +1,27 @@
+'''
+Написати програму яка б почергово оновлювала дані (def updat)
+та передбачала місцеположення (def predict), використовуючи
+вимірювання та параметри руху, що наведені нижче
+'''
+
+def update(mean1, var1, mean2, var2):
+    new_mean = float(var2 * mean1 + var1 * mean2) / (var1 + var2)
+    new_var = 1./(1./var1 + 1./var2)
+    return [new_mean, new_var]
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+measurements = [5., 6., 7., 9., 10.]
+motion = [1., 1., 2., 1., 1.]
+measurement_sig = 4.
+motion_sig = 2.
+mu = 0.
+sig = 10000.
+
+# ваш код тут
+
+print(mu, sig)
+
